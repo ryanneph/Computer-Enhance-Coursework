9dcd8ca710c4be00488865f137486a2b25ed6bfd
Ryan Neph <neph320@gmail.com>
2024-04-20 00:05:10 -0700

    execution_ports challenge hw

--- 02-x64_experiments/execution_ports.asm
+++ 02-x64_experiments/execution_ports.asm
@@ -2,6 +2,8 @@ global read_x1
 global read_x2
 global read_x3
 global read_x4
+global read_1x2
+global read_8x2
 
 section .text
 
@@ -42,5 +44,23 @@ read_x4:
     mov rax, [rdi]
     mov rax, [rdi]
     sub rsi, 4
+    jnle .loop
+    ret
+
+read_1x2:
+    align 64
+.loop:
+    mov al, [rdi]
+    mov al, [rdi]
+    sub rsi, 2
+    jnle .loop
+    ret
+
+read_8x2:
+    align 64
+.loop:
+    mov rax, [rdi]
+    mov rax, [rdi]
+    sub rsi, 2
     jnle .loop
     ret
