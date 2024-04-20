7820d341b325a44e5041391d484fc51f7bed42f9
Ryan Neph <neph320@gmail.com>
2024-04-19 23:57:14 -0700

    add execution_ports test

---
+++ 02-x64_experiments/execution_ports.asm
@@ -0,0 +1,46 @@
+global read_x1
+global read_x2
+global read_x3
+global read_x4
+
+section .text
+
+; rdi - data pointer
+; rsi - count
+read_x1:
+    align 64
+.loop:
+    mov rax, [rdi]
+    sub rsi, 1
+    jnle .loop
+    ret
+
+read_x2:
+    align 64
+.loop:
+    mov rax, [rdi]
+    mov rax, [rdi]
+    sub rsi, 2
+    jnle .loop
+    ret
+
+read_x3:
+    align 64
+.loop:
+    mov rax, [rdi]
+    mov rax, [rdi]
+    mov rax, [rdi]
+    sub rsi, 3
+    jnle .loop
+    ret
+
+read_x4:
+    align 64
+.loop:
+    mov rax, [rdi]
+    mov rax, [rdi]
+    mov rax, [rdi]
+    mov rax, [rdi]
+    sub rsi, 4
+    jnle .loop
+    ret
