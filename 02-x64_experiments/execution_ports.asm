06bd7cc70d3be4de559f305023f345b76fa5004a
Ryan Neph <neph320@gmail.com>
2024-04-23 23:08:37 -0700

    execution_ports: test write port count

--- 02-x64_experiments/execution_ports.asm
+++ 02-x64_experiments/execution_ports.asm
@@ -1,3 +1,6 @@
+;
+; LOAD
+;
 global read_8x1
 global read_8x2
 global read_8x3
@@ -51,6 +54,66 @@ read_1x16:
 .loop:
 %rep 16
     mov al, [rdi]
+%endrep
+    sub rsi, 16
+    jnle .loop
+    ret
+
+
+;
+; STORE
+;
+global write_8x1
+global write_8x2
+global write_8x3
+global write_8x4
+global write_1x16
+
+; rdi - data pointer
+; rsi - count
+write_8x1:
+    align 64
+.loop:
+    mov [rdi], rax
+    sub rsi, 8
+    jnle .loop
+    ret
+
+write_8x2:
+    align 64
+.loop:
+    mov [rdi], rax
+    mov [rdi], rax
+    sub rsi, 16
+    jnle .loop
+    ret
+
+write_8x3:
+    align 64
+.loop:
+    mov [rdi], rax
+    mov [rdi], rax
+    mov [rdi], rax
+    sub rsi, 24
+    jnle .loop
+    ret
+
+write_8x4:
+    align 64
+.loop:
+    mov [rdi], rax
+    mov [rdi], rax
+    mov [rdi], rax
+    mov [rdi], rax
+    sub rsi, 32
+    jnle .loop
+    ret
+
+write_1x16:
+    align 64
+.loop:
+%rep 16
+    mov [rdi], al
 %endrep
     sub rsi, 16
     jnle .loop
