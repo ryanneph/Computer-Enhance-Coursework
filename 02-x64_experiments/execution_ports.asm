976e02da398d194a807cfbe62d7cb2f38c3a0e6f
Ryan Neph <neph320@gmail.com>
2024-04-23 22:55:49 -0700

    execution_ports: correct memory bandwidth readouts

--- 02-x64_experiments/execution_ports.asm
+++ 02-x64_experiments/execution_ports.asm
@@ -1,58 +1,18 @@
-global read_x1
-global read_x2
-global read_x3
-global read_x4
-global read_1x2
+global read_8x1
 global read_8x2
+global read_8x3
+global read_8x4
+global read_1x16
 
 section .text
 
 ; rdi - data pointer
 ; rsi - count
-read_x1:
+read_8x1:
     align 64
 .loop:
     mov rax, [rdi]
-    sub rsi, 1
-    jnle .loop
-    ret
-
-read_x2:
-    align 64
-.loop:
-    mov rax, [rdi]
-    mov rax, [rdi]
-    sub rsi, 2
-    jnle .loop
-    ret
-
-read_x3:
-    align 64
-.loop:
-    mov rax, [rdi]
-    mov rax, [rdi]
-    mov rax, [rdi]
-    sub rsi, 3
-    jnle .loop
-    ret
-
-read_x4:
-    align 64
-.loop:
-    mov rax, [rdi]
-    mov rax, [rdi]
-    mov rax, [rdi]
-    mov rax, [rdi]
-    sub rsi, 4
-    jnle .loop
-    ret
-
-read_1x2:
-    align 64
-.loop:
-    mov al, [rdi]
-    mov al, [rdi]
-    sub rsi, 2
+    sub rsi, 8
     jnle .loop
     ret
 
@@ -61,6 +21,37 @@ read_8x2:
 .loop:
     mov rax, [rdi]
     mov rax, [rdi]
-    sub rsi, 2
+    sub rsi, 16
+    jnle .loop
+    ret
+
+read_8x3:
+    align 64
+.loop:
+    mov rax, [rdi]
+    mov rax, [rdi]
+    mov rax, [rdi]
+    sub rsi, 24
+    jnle .loop
+    ret
+
+read_8x4:
+    align 64
+.loop:
+    mov rax, [rdi]
+    mov rax, [rdi]
+    mov rax, [rdi]
+    mov rax, [rdi]
+    sub rsi, 32
+    jnle .loop
+    ret
+
+read_1x16:
+    align 64
+.loop:
+%rep 16
+    mov al, [rdi]
+%endrep
+    sub rsi, 16
     jnle .loop
     ret
