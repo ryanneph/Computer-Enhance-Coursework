a867f1c39550b6afbc448231ea030554127d4ae5
Ryan Neph <neph320@gmail.com>
2024-04-27 15:26:33 -0700

    cache_sizes: double the moves-per-loop to hide dependency chain

--- 02-x64_experiments/cache_sizes.asm
+++ 02-x64_experiments/cache_sizes.asm
@@ -1,22 +1,37 @@
-global read_wrapping_pow2
+global read_wrapping_pow2_32x8
 
 section .text
 
 ; rdi - read base pointer
 ; rsi - read byte count
 ; rdx - read offset mask (must be pow2)
-read_wrapping_pow2:
+read_wrapping_pow2_32x8:
     ; 32-byte-wide SIMD read from base pointer + masked offset to register
-    ; rax - read offset
+    ; r10 - read offset
+    ; r11 - pointer
     align 64
-    xor rax, rax
+    xor r10, r10
 .loop:
-    vmovdqu ymm0, [rdi + rax]
-    vmovdqu ymm0, [rdi + rax + 32]
-    vmovdqu ymm0, [rdi + rax + 64]
-    vmovdqu ymm0, [rdi + rax + 96]
-    add rax, 128
-    and rax, rdx
-    sub rsi, 128
+    ; compute pointer
+    mov r11, rdi
+    add r11, r10
+
+    ; move the stuff
+    vmovdqu ymm0, [r11       ]
+    vmovdqu ymm0, [r11 + 0x20]
+    vmovdqu ymm0, [r11 + 0x40]
+    vmovdqu ymm0, [r11 + 0x60]
+
+    vmovdqu ymm0, [r11 + 0x80]
+    vmovdqu ymm0, [r11 + 0xa0]
+    vmovdqu ymm0, [r11 + 0xc0]
+    vmovdqu ymm0, [r11 + 0xe0]
+
+    ; advance offset
+    add r10, 0x100
+    and r10, rdx
+
+    ; decrement counter
+    sub rsi, 0x100
     jnle .loop
     ret
