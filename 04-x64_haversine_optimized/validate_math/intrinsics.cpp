37b85737e8caf5af26d81c146c6a1087ee591af0
Ryan Neph <neph320@gmail.com>
2025-02-01 10:50:44 -0800

    add asin approximations

--- 04-x64_haversine_optimized/validate_math/intrinsics.cpp
+++ 04-x64_haversine_optimized/validate_math/intrinsics.cpp
@@ -2,21 +2,21 @@
 
 extern "C" {
 
-double sqrt_intrinsic_cpp(double x) {
+double sqrt_intrin_cpp(double x) {
    __m128d input = _mm_set_sd(x);
    __m128d computed = _mm_sqrt_sd(input, input);
    double output = _mm_cvtsd_f64(computed);
    return output;
 }
 
-double sqrt_intrinsic_f32_precision_cpp(double x) {
+double sqrt_intrin_f32_cpp(double x) {
    __m128d input = _mm_set_ss((float)x);
    __m128d computed = _mm_sqrt_ss(input);
    double output = _mm_cvtss_f32(computed);
    return output;
 }
 
-double sqrt_intrinsic_reciprocal_cpp(double x) {
+double sqrt_intrin_rcp_cpp(double x) {
    __m128d input = _mm_set_ss((float)x);
    __m128d computed = _mm_rsqrt_ss(input);
    computed = _mm_rcp_ss(computed);
@@ -25,7 +25,7 @@ double sqrt_intrinsic_reciprocal_cpp(double x) {
 }
 
 #define PI 3.141592653589793
-double sin_quad_zero_to_pi(double input) {
+double sinq_zero_to_pi_cpp(double input) {
    __m128d A = _mm_set_sd(-4.0 / (PI * PI));
    __m128d B = _mm_set_sd(4.0 / PI);
    __m128d x = _mm_set_sd(input);
@@ -36,6 +36,55 @@ double sin_quad_zero_to_pi(double input) {
    __m128 computed = _mm_add_sd(term1, term2);
 
    double result = _mm_cvtsd_f64(computed);
+   return result;
+}
+
+double sinq_intrin_cpp(double x) {
+   double sign = 1.0;
+   if (x < 0) {
+      x = -x;
+      sign = -1.0;
+   }
+
+   double result = sign * sinq_zero_to_pi_cpp(x);
+   return result;
+}
+
+double cosq_intrin_cpp(double x) {
+   x = x + (PI / 2.0);
+   double result = sinq_intrin_cpp(x);
+   return result;
+}
+
+double asin_intrin1_cpp(double input) {
+   // ops: 5 (4 cheap + 1 expensive)
+   //   mul: 3
+   //   add: 1
+   //   div: 1
+   double x = input;
+   double result = x + (1.0/6.0) * (x*x*x);
+   return result;
+}
+
+double asin_intrin2_cpp(double input) {
+   // ops: 6 (4 cheap, 2 expensive)
+   //   mul:  2
+   //   add:  2
+   //   div:  1
+   //   sqrt: 1
+   double x = input;
+   double result = (2/x) * (1 - sqrt_intrin_cpp(1 - x*x));
+   return result;
+}
+
+double asin_intrin3_cpp(double input) {
+   // ops: 9 (7 cheap, 2 expensive)
+   //   mul:  4
+   //   add:  3
+   //   div:  1
+   //   sqrt: 1
+   double x = input;
+   double result = (PI/2.0) * (0.136*x + (1/x)*(1-sqrt_intrin_cpp(1-x*x)));
    return result;
 }
 
