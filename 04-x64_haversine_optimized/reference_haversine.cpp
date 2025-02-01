bbdb26fb2c908cd9b5f0d60c5c8dfe047f39d6a0
Ryan Neph <neph320@gmail.com>
2025-02-01 11:11:30 -0800

    add ReferenceHaversineApproxCpp repetition test

--- 04-x64_haversine_optimized/reference_haversine.cpp
+++ 04-x64_haversine_optimized/reference_haversine.cpp
@@ -1,4 +1,6 @@
 #include <math.h>
+#include "validate_math/intrinsics.cpp"
+
 extern "C" {
 
 struct Point_Pair {
@@ -14,6 +16,22 @@ static inline double square(double a) {
    return result;
 }
 
+static double sin_(double x) {
+   return sinq_intrin_cpp(x);
+}
+
+static double cos_(double x) {
+   return cosq_intrin_cpp(x);
+}
+
+static double asin_(double x) {
+   return asin_intrin3_cpp(x);
+}
+
+static double sqrt_(double x) {
+   return sqrt_intrin_cpp(x);
+}
+
 double reference_haversine_cpp(Point_Pair pair, double earth_radius) {
    double lat0 = pair.y0;
    double lat1 = pair.y1;
@@ -27,6 +45,24 @@ double reference_haversine_cpp(Point_Pair pair, double earth_radius) {
 
    double a = square(sin(dlat / 2.0)) + cos(lat0) * cos(lat1) * square(sin(dlon / 2.0));
    double c = 2.0 * asin(sqrt(a));
+
+   double result = earth_radius * c;
+   return result;
+}
+
+double reference_haversine_approx_cpp(Point_Pair pair, double earth_radius) {
+   double lat0 = pair.y0;
+   double lat1 = pair.y1;
+   double lon0 = pair.x0;
+   double lon1 = pair.x1;
+
+   double dlat = radians(lat1 - lat0);
+   double dlon = radians(lon1 - lon0);
+   lat0 = radians(lat0);
+   lat1 = radians(lat1);
+
+   double a = square(sin_(dlat / 2.0)) + cos_(lat0) * cos_(lat1) * square(sin_(dlon / 2.0));
+   double c = 2.0 * asin_(sqrt_(a));
 
    double result = earth_radius * c;
    return result;
